e21a8415c51b1e37b3897b7993f294a828d9b2d5
jjvs92 <julian_jvs92@hotmail.com>
2018-08-15 23:32:47 -0500

    added instructions component

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import Header from "./components/Header";
-import Instructions from "./components/instructions";
+import Instructions from "./components/Instructions";
 
 
 class App extends Component {
+  state= {
+    centerText: "Click an image to begin!"
+  }
+
+
   render() {
     return (
       <div>
--- memory-game/src/components/instructions.js
+++ memory-game/src/components/instructions.js
@@ -2,8 +2,10 @@ import React from "react";
 import "./css/instructions.css";
 
 const Instructions = props => 
-
-        <div>{props.children} testing</div>
+        <div class="titleHome">
+                <h1 id="title">Clicky Game!</h1>
+                <h4 id="instructions">Click on an image to earn points, but don't click on any more than once!</h4>
+        </div>
     
 
 export default Instructions;
